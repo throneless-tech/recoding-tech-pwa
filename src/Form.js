f1073aef26be91ea72d235b92ae90222992d732d
rudietuesdays <rae@throneless.tech>
2021-04-28 17:26:28 -0400

    add form and validation to front end

--- src/Form.js
+++ src/Form.js
@@ -1,19 +1,105 @@
-// /src/MadLib.js
+import React from 'react';
 import { useQuery } from 'react-query';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import BlockContent from '@sanity/block-content-to-react';
 import { sanity } from './sanity';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 // Material UI imports
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+
+// images
+import keys from './svgs/keys.svg';
+
+const useStyles = makeStyles(theme => ({
+  border: {
+    position: 'relative',
+    '&:after': {
+      backgroundColor: '#F6F000',
+      bottom: '-80px',
+      content: '""',
+      height: 36,
+      left: 0,
+      position: 'absolute',
+      right: 0,
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        bottom: 0,
+        position: 'fixed',
+      },
+    }
+  },
+  help: {
+    color: theme.palette.error.main,
+  },
+  img: {
+    display: 'block',
+    marginLeft: '-70px',
+    minWidth: 600,
+    objectFit: 'cover',
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      marginLeft: 0,
+    },
+  },
+  imgContainer: {
+    backgroundColor: '#F6F000',
+    maxHeight: 100,
+    overflow: 'hidden',
+    [theme.breakpoints.up('md')]: {
+      maxHeight: 150,
+    },
+    [theme.breakpoints.up('lg')]: {
+      maxHeight: 200,
+    },
+  },
+  input: {
+    padding: 5,
+    width: '100%',
+  }
+}));
 
 const query = `
   *[ _type == 'signup' ] { email, background, location, policy, uses }
 `;
 
 function Signup() {
+  const classes = useStyles();
+
+  // validation for the form fields
+  const validationSchema = Yup.object({
+    email: Yup.string().email().required(),
+    background: Yup.string().required(),
+    location: Yup.string().required(),
+    policy: Yup.string().required(),
+    uses: Yup.string().required(),
+  });
+
+  // start off fields as blank
+  const initialValues = {
+    email: "",
+    background: "",
+    location: "",
+    policy: "",
+    uses: "",
+  };
+
+  // what do on form submit FIXME
+  const onSubmit = (values) => {
+    alert(JSON.stringify(values, null, 2));
+  };
+
+
+  const renderError = (message) => <p className={classes.help}>{message}</p>;
+
   // in this one line, data is fetched from sanity via the sanity client and
   // stored into application state via react-query!
   const { data: signup } = useQuery('signup', () => sanity.fetch(query));
@@ -40,79 +126,100 @@ function Signup() {
     return <h1>Loading…</h1>;
   }
 
-  // once the sign up form is loaded, we can map through the structured content to
-  // find our placeholder shape. the end result is an array of these placeholders
-  const placeholders = signup
-    .map((block) => block.children.filter((n) => n._type === 'placeholder'))
-    .flat();
-
-  // using the above placeholders array, we calculate whether or not all the
-  // blanks are filled in by checking the whether every placeholder has a value
-  // in the `answers` state variable.
-  const allBlanksFilledIn = placeholders?.every(
-    (placeholder) => answers[placeholder._key],
-  );
-
   return (
-    <>
-      <Typography component="h1" variant="h1">Sign up</Typography>
-      {!allBlanksFilledIn ? (
-        // if all the blanks are _not_ filled in, then we can show the form
-        <>
-          <p>Fill in the form.</p>
-          <form
-            // this `onSubmit` will fire when the user clicks the submit button
-            onSubmit={(e) => {
-              e.preventDefault();
-
-              const answerEntries = Array.from(
-                // find all the inputs
-                e.currentTarget.querySelectorAll('input'),
-              )
-                // then get the name and values in a tuple
-                .map((inputEl) => [inputEl.name, inputEl.value]);
-
-              // use `Object.fromEntries` to transform them back to an object
-              const nextAnswers = Object.fromEntries(answerEntries);
-
-              setAnswers(nextAnswers);
+    <Box className={classes.border}>
+      <Box className={classes.imgContainer}>
+        <img src={keys} alt="" aria-hidden="true" className={classes.img} />
+      </Box>
+      <Box mt={2} mb={6}>
+        <Container>
+          <Typography component="h1" variant="h1">Sign up</Typography>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={async (values, { resetForm }) => {
+              await onSubmit(values);
+              resetForm();
             }}
           >
-            <ul>
-              {/* for each placeholder… */}
-              {placeholders.map(({ _key, type }) => (
-                <li key={_key}>
-                  {/* …render an input an a label. */}
-                  <input
-                    // the `name` of the input will be the sanity `_key`:
-                    // https://www.sanity.io/docs/array-type#why-the-key-92296c6c45ea
-                    // this will enables us to match this input value with the
-                    // correct placeholder
-                    name={_key}
-                    id={_key}
-                  />
-                  <label htmlFor={_key}>
-                    {type}
+            <Form>
+              <Box my={4}>
+                <Box mb={2}>
+                  <label className={classes.label} htmlFor="email">
+                    Email address
                   </label>
-                </li>
-              ))}
-            </ul>
-            <button>Submit!</button>
-          </form>
-        </>
-      ) : (
-        // if all the blanks are filled in, then we can show the rendered
-        // story with a custom serializer for the type `placeholder`
-        <>
-          <BlockContent
-            blocks={signup.story}
-            serializers={{
-              // see here: https://github.com/sanity-io/block-content-to-react
-              types: { placeholder: ({ node: { _key } }) => answers[_key] },
-            }}
-          />
+                  <div>
+                    <Field
+                      name="email"
+                      type="text"
+                      placeholder="me@example.com"
+                      className={classes.input}
+                    />
+                    <ErrorMessage name="email" render={renderError} />
+                  </div>
+                </Box>
+                <Box mb={2}>
+                  <label className={classes.label} htmlFor="background">
+                    Background or field
+                  </label>
+                  <div>
+                    <Field
+                      name="background"
+                      type="text"
+                      className={classes.input}
+                    />
+                    <ErrorMessage name="background" render={renderError} />
+                  </div>
+                </Box>
+                <Box mb={2}>
+                  <label className={classes.label} htmlFor="location">
+                    Countries or geographies of interest
+                  </label>
+                  <div>
+                    <Field
+                      name="location"
+                      type="text"
+                      className={classes.input}
+                    />
+                    <ErrorMessage name="location" render={renderError} />
+                  </div>
+                </Box>
+                <Box mb={2}>
+                  <label className={classes.label} htmlFor="policy">
+                    Tech policy areas of interest
+                  </label>
+                  <div>
+                    <Field
+                      name="policy"
+                      type="text"
+                      className={classes.input}
+                    />
+                    <ErrorMessage name="policy" render={renderError} />
+                  </div>
+                </Box>
+                <Box mb={2}>
+                  <label className={classes.label} htmlFor="uses">
+                    How could a resource like recoding.tech be most useful to you and your work?
+                  </label>
+                  <div>
+                    <Field
+                      name="uses"
+                      type="text"
+                      className={classes.input}
+                    />
+                    <ErrorMessage name="uses" render={renderError} />
+                  </div>
+                </Box>
 
-          <button
+                <button type="submit" className="button is-primary">
+                  Submit
+                </button>
+              </Box>
+            </Form>
+          </Formik>
+          <Button
+            color="primary"
+            variant="contained"
             onClick={() => {
               // we reset the state on click after the users confirms it's okay.
               if (window.confirm('Are you sure you want to reset?')) {
@@ -121,15 +228,15 @@ function Signup() {
             }}
           >
             Reset
-          </button>
+          </Button>
 
-          {/* this is a simple link back to the main mab libs index */}
-          <Link to="/">
-            ← More Mad Libs
+          {/* this is a simple link back to the homepage */}
+          <Link href="/">
+            ← Back
           </Link>
-        </>
-      )}
-    </>
+        </Container>
+      </Box>
+    </Box>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,18 @@
 // base imports
+import React from 'react';
 import { useQuery } from 'react-query';
 import { sanity } from './sanity';
 
 // Material UI imports
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
-import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
 // images
 import keys from './svgs/keys.svg';
-
-const query = `
-  *[ _type == 'madLib' ] { title, image, slug }
-`;
 
 const useStyles = makeStyles(theme => ({
   bold: {
